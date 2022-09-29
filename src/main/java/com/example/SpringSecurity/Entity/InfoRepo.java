89228b3e06c1837335d56a673d896552d37c12ea
sodmod <horluwatosin1999@gmail.com>
2022-09-29 08:20:21 +0100

    Permissions

--- src/main/java/com/example/SpringSecurity/Entity/InfoRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.SpringSecurity.Entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InfoRepo extends JpaRepository<Info, Long> {
-
-}
